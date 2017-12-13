78a3fe1f8e3900949b22e4e6883b90e00678edb5
SelFone <xal_xc@hotmail.com>
2017-12-13 23:56:51 +0800

    bug版

--- app/src/main/java/com/coolweather/test/ChooseAreaFrament.java
+++ app/src/main/java/com/coolweather/test/ChooseAreaFrament.java
@@ -2,6 +2,7 @@ package com.coolweather.test;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -16,11 +17,11 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.administrator.coolweather.db.City;
-import com.example.administrator.coolweather.db.County;
-import com.example.administrator.coolweather.db.Province;
-import com.example.administrator.coolweather.util.HttpUtil;
-import com.example.administrator.coolweather.util.Utility;
+import com.coolweather.test.db.City;
+import com.coolweather.test.db.County;
+import com.coolweather.test.db.Province;
+import com.coolweather.test.util.HttpUtil;
+import com.coolweather.test.util.Utility;
 
 import org.litepal.crud.DataSupport;
 
@@ -36,7 +37,7 @@ import okhttp3.Response;
  * Created by Administrator on 2017/11/27.
  */
 
-public class ChooseAreaFrament extends Fragment {
+public class ChooseAreaFrament extends android.support.v4.app.Fragment {
     public static final int LEVEL_PROVINCE =0;
     public static final int LEVEL_CITY =1;
     public static final int LEVEL_COUNTY = 2;
@@ -59,7 +60,7 @@ public class ChooseAreaFrament extends Fragment {
     //当前选中的级别
     private int currentLevel;
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -67,7 +68,7 @@ public class ChooseAreaFrament extends Fragment {
         titleText = (TextView) view.findViewById(R.id.title_text);
         backButton = (Button) view.findViewById(R.id.back_button);
         listView = (ListView) view.findViewById(R.id.list_view);
-        adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_expandable_list_item_1,dataList);
+        adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
         listView.setAdapter(adapter);
         return view;
     }
@@ -84,6 +85,20 @@ public class ChooseAreaFrament extends Fragment {
                 }else if(currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    if (getActivity() instanceof  MainActivity){
+                        Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                        intent.putExtra("weather_id",weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }else if (getActivity() instanceof WeatherActivity){
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
+
                 }
             }
         });
@@ -120,10 +135,10 @@ public class ChooseAreaFrament extends Fragment {
 
 
     //查询全国所有的市，优先从数据库查询，如果没有查询到的再去服务器上查询
-    private void queryCounties() {
+    private void queryCities() {
         titleText.setText(selectedProvince.getProvinceName());
         backButton.setVisibility(View.VISIBLE);
-        cityList = DataSupport.where("province = ?",String.valueOf(selectedProvince.getId())).find(City.class);
+        cityList = DataSupport.where("provinceid = ?",String.valueOf(selectedProvince.getId())).find(City.class);
         if (cityList.size() > 0){
             dataList.clear();
             for (City city : cityList){
@@ -140,10 +155,11 @@ public class ChooseAreaFrament extends Fragment {
     }
 
     //查询全国所有的县，优先从数据库查询，如果没有查询到的再去服务器上查询
-    private void queryCities() {
+    private void  queryCounties() {
         titleText.setText(selectedProvince.getProvinceName());
         backButton.setVisibility(View.VISIBLE);
-        countyList = DataSupport.where("cityid = ?",String.valueOf(selectedCity.getId())).find(County.class);
+        countyList = DataSupport.where("cityid = ?",String.valueOf(selectedCity.getId())).
+                find(County.class);
         if (countyList.size() > 0){
             dataList.clear();
             for (County county : countyList){
@@ -163,17 +179,6 @@ public class ChooseAreaFrament extends Fragment {
     private void queryFromServer(String address,final String type) {
         showProgressDialog();
         HttpUtil.sendOkHttpRequest(address, new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                //通过runOnUiThread()方法回到主线程处理逻辑
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        closeProgressDialog();
-                        Toast.makeText(getContext(),"加载失败",Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
@@ -184,7 +189,7 @@ public class ChooseAreaFrament extends Fragment {
                 }else if("city".equals(type)) {
                     result = Utility.handleCityResponse(responseText,selectedProvince.getId());
                 }else if("county".equals(type)) {
-                    result = Utility.handleCountyResponse(responseText,selectedProvince.getId());
+                    result = Utility.handleCountyResponse(responseText,selectedCity.getId());
                 }
                 if (result){
                     getActivity().runOnUiThread(new Runnable() {
@@ -201,6 +206,18 @@ public class ChooseAreaFrament extends Fragment {
                         }
                     });
                 }
+            }
+            @Override
+            public void onFailure(Call call, IOException e) {
+                //通过runOnUiThread()方法回到主线程处理逻辑
+                getActivity().runOnUiThread(new Runnable() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(),"加载失败",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/coolweather/test/util/Utility.java
+++ app/src/main/java/com/coolweather/test/util/Utility.java
@@ -2,9 +2,11 @@ package com.coolweather.test.util;
 
 import android.text.TextUtils;
 
-import com.example.administrator.coolweather.db.City;
-import com.example.administrator.coolweather.db.County;
-import com.example.administrator.coolweather.db.Province;
+import com.coolweather.test.db.City;
+import com.coolweather.test.db.County;
+import com.coolweather.test.db.Province;
+import com.coolweather.test.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -71,5 +73,18 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    //将返回的JSON数据解析成Weather实体类
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
